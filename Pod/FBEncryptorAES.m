9a95f3f0fe324893cd2cc4959d8def3c54a22427
Bogdan Laukhin <blaukhin@plexteq.com>
2017-03-27 15:32:54 +0300

    fixed minor defects

--- Pod/FBEncryptorAES.m
+++ Pod/FBEncryptorAES.m
@@ -152,7 +152,7 @@
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        srand(time(NULL));
+        srand((int)time(NULL));
     });
 
     char cIv[FBENCRYPT_BLOCK_SIZE];
